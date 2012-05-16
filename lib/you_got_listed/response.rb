f86d2564989fac7d96d7abb49f93aa4ddeeb1be0
Tom Cocca <tom.cocca@gmail.com>
2012-05-16 09:25:29 -0400

    rescue a MultiXml::ParseError and set the response to nil so it is not a success

--- lib/you_got_listed/response.rb
+++ lib/you_got_listed/response.rb
@@ -4,19 +4,24 @@ module YouGotListed
     attr_accessor :ygl_response
 
     def initialize(response, raise_error = true)
-      if !response.respond_to?(:each_pair)
-        self.ygl_response = nil
-        raise Error.new('empty_response', 'Empty Response') if raise_error
-      else
-        rash = Hashie::Rash.new(response)
-        self.ygl_response = rash.ygl_response
-        if !success? && raise_error
-          if self.ygl_response.respond_to?(:response_code)
-            raise Error.new(self.ygl_response.response_code, self.ygl_response.error)
-          else
-            raise Error.new('empty_response', 'Empty Response')
+      begin
+        if !response.respond_to?(:each_pair)
+          self.ygl_response = nil
+          raise Error.new('empty_response', 'Empty Response') if raise_error
+        else
+          rash = Hashie::Rash.new(response)
+          self.ygl_response = rash.ygl_response
+          if !success? && raise_error
+            if self.ygl_response.respond_to?(:response_code)
+              raise Error.new(self.ygl_response.response_code, self.ygl_response.error)
+            else
+              raise Error.new('empty_response', 'Empty Response')
+            end
           end
         end
+      rescue MultiXml::ParseError
+        self.ygl_response = nil
+        raise Error.new('parse_error', 'XML Parse Error') if raise_error
       end
     end
 
